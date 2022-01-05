1fbe34a6f0b1ccc580fec85ff354c5dd083ad048
DarioDiGulio <ddigulio@proyecto404.com>
2022-01-04 22:27:48 -0300

    Substract Method

--- Calculator.cs
+++ Calculator.cs
@@ -1,8 +1,14 @@
-﻿namespace Math {
+﻿using System;
+
+namespace Math {
     public class Calculator {
         public Calculator() {}
         public int Add(int  number1, int number2) {
             return number1 + number2;
+        }
+
+        public int Substract(int number1, int number2) {
+            return number1 - number2;
         }
     }
 }
